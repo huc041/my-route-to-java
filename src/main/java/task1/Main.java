390afe156cfe8b6171a09821fcc4b1abc8a48d50
huc041 <jovanny041@gmail.com>
2026-07-13 12:19:41 +0300

    Update task1 solution

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         //String s = "J@va the be$t!123"; // →"t@eb eht av$J!123";
-        String s = "C0de_@ Rev!ew99"; // fteb eht av$a!1J3
+        //String s = "C0de_@ Rev!ew99"; // fteb eht av$a!1J3
         //String s = "Int3l#J Cor@2026x";
+        String s = ""; // fteb eht av$a!1J3
         if (s.isEmpty() || s.length() <=1) {
             System.out.println("Строка слишком короткая или пустая");
+            return;
         }
         else {
                 int n = s.length();        // 4
