1691944bf2354b112d8467b4abb91bba33fba4eb
huc041 <jovanny041@gmail.com>
2026-07-16 08:31:38 +0300

    use class's method instead of intance's method

--- src/test/java/com/learning/util/StringUtilTest.java
+++ src/test/java/com/learning/util/StringUtilTest.java
@@ -6,47 +6,45 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringUtilTest {
 
-    private final StringUtil strUtil = new StringUtil();
-
     @Test
     void shouldReverseLettersKeepingSymbols() {
-        String result = strUtil.reverseLetters("J@va the be$t!123");
+        String result = StringUtil.reverseLetters("J@va the be$t!123");
         assertEquals("t@eb eht av$J!123", result);
     }
 
     @Test
     void returnsEmptyForEmptyInput(){
-        String result = strUtil.reverseLetters("");
+        String result = StringUtil.reverseLetters("");
         assertEquals("", result);
     }
 
     @Test
     void reversesOnlyOneLetter(){
-        String result = strUtil.reverseLetters("a");
+        String result = StringUtil.reverseLetters("a");
         assertEquals("a", result);
     }
 
     @Test
     void KeepNonLettersInPlace(){
-        String result = strUtil.reverseLetters("123 !@#");
+        String result = StringUtil.reverseLetters("123 !@#");
         assertEquals("123 !@#", result);
     }
 
     @Test
     void reversesOnlyLetters(){
-        String result = strUtil.reverseLetters("abcd");
+        String result = StringUtil.reverseLetters("abcd");
         assertEquals("dcba", result);
     }
 
     @Test
     void KeepNonLettersInEdgesAndInMiddle(){
-        String result = strUtil.reverseLetters("!ab4cd?");
+        String result = StringUtil.reverseLetters("!ab4cd?");
         assertEquals("!dc4ba?", result);
     }
 
     @Test
     void reversesLettersWithCorrectRegister(){
-        String result = strUtil.reverseLetters("aBcD");
+        String result = StringUtil.reverseLetters("aBcD");
         assertEquals("DcBa", result);
     }
 
@@ -54,7 +52,7 @@ public class StringUtilTest {
     void inputNULLString(){
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
-                () -> strUtil.reverseLetters(null)
+                () -> StringUtil.reverseLetters(null)
         );
         assertEquals("Строка не инициализирована (null)", exception.getMessage());
     }
